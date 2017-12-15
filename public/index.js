c0420d9c9414d3afbb5ec3c679a52221442c943b
Laurence <ljwoodward.code@gmail.com>
2017-12-15 22:03:37 +0000

    added descriptions

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
-const responseUrl = "https://newsapi.org/v2/everything?q=ai&sortBy=publishedAt&apiKey=6f80860ee9974d0d8301b6bce7fa9e8d";
-let aiArticles = [];
+const responseUrl = "https://newsapi.org/v2/everything?q=javascript&sortBy=popularity&apiKey=6f80860ee9974d0d8301b6bce7fa9e8d";
+let articles = [];
 
 const app = function(){
   makeRequest(responseUrl, function () {
     if (this.status !== 200) return;
     const jsonString = this.responseText;
     const response = JSON.parse(jsonString);
-    aiArticles = response.articles;
-    render(aiArticles);
+    articles = response.articles;
+    render(articles);
   });
 }
 
@@ -21,11 +21,17 @@ const makeRequest = function (responseUrl, callback) {
 const render = function(articles) {
   articles.forEach(function(article) {
     const table = document.querySelector('#ai-articles');
-    const row = document.createElement('tr');
+    const headerRow = document.createElement('tr');
     const header = document.createElement('td');
-    header.innerText = article.title;
-    row.appendChild(header);
-    table.appendChild(row);
+    header.innerHTML = `<h2>${article.title}</h2>`;
+    headerRow.appendChild(header);
+    const descriptionRow = document.createElement('tr');
+    const description = document.createElement('td');
+    description.innerText = article.description;
+    descriptionRow.appendChild(description);
+    table.appendChild(headerRow);
+    table.appendChild(descriptionRow);
+
   })
 }
 
